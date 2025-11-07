048262782aa28af8b563896f39a264c6dddc042a
Voiteg <twojwujek46@gmail.com>
2025-11-07 13:55:27 +0100

    Logowanie + rejestracja dzialajaca wersja

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Login from "./components/Login.jsx";
 import Rejestracja from "./components/Rejestracja.jsx";
 
+function App() {
+  const [pokazLogin, setPokazLogin] = useState(true);
+
+  return (
+    <div className="flex flex-col items-center gap-4 p-6">
+      {pokazLogin ? (
+        <Login onSwitch={() => setPokazLogin(false)} />
+      ) : (
+        <Rejestracja onSwitch={() => setPokazLogin(true)} />
+      )}
+    </div>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("app")).render(
-    <React.StrictMode>
-        <Rejestracja></Rejestracja>
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
